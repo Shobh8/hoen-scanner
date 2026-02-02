05eb7c08b120b5b32a266ac31bff79d0eb740edb
Shobh8 <134914938+Shobh8@users.noreply.github.com>
2026-02-02 16:20:55 +0530

    Create SearchResult.java

---
+++ src/main/java/com/hoenscanner/core/SearchResult.java
@@ -0,0 +1,29 @@
+package com.hoenscanner.core;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SearchResult {
+
+    @JsonProperty
+    private String city;
+
+    @JsonProperty
+    private String kind;
+
+    @JsonProperty
+    private String title;
+
+    public SearchResult() {}
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
